2538444e7cc53fd46c28c984ff39facff5ac9604
Ben <bdye71@gmail.com>
2020-07-18 14:25:54 -0400

    Backend--set Card model 'type' field default value of 1

--- backend/cards/models.py
+++ backend/cards/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Card(models.Model):
-    type = models.PositiveSmallIntegerField()
+    type = models.PositiveSmallIntegerField(default=1) # Default to 1 = "general" for now.
     front = models.TextField()
     back = models.TextField()
     known = models.BooleanField(default=False)
