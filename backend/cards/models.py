ccdcc38cfa024b87660665a2cd082ff7ca85fcf7
Ben <bdye71@gmail.com>
2020-09-21 16:17:37 -0400

    Added user id field as ForeignKey on Card model, seems to have migrated ok. Set default value to 1 for all existing cards using the django migrations command prompt, since that's the correct uid for all existing cards.

--- backend/cards/models.py
+++ backend/cards/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Card(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     type = models.PositiveSmallIntegerField(default=1) # Default to 1 = "general" for now.
     front = models.TextField()
     back = models.TextField()
--- backend/cards/tests.py
+++ backend/cards/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase # this is some Django-tailored subclass of unittest.TestCase (?)
 
-# Create your tests here.
+from .models import Card
+
+class CardModelTests(TestCase):
+
+    def setUp(self):
+        pass
+
+        # self.user = get_user_model().objects.create_user(
+        #     username="mockuser",
+        #     password="testpass123"
+        # )
+
+    def test_that_testing_works(self):
+        self.assertEqual(2 + 2, 4)
+
