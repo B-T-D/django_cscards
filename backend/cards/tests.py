58dbecd757732b72e1add5b6f3f9099677e30ae8
Ben <bdye71@gmail.com>
2020-09-21 16:23:53 -0400

    Quick anti-breakage test for cards/models.py

--- backend/cards/tests.py
+++ backend/cards/tests.py
@@ -1,17 +1,21 @@
 from django.test import TestCase # this is some Django-tailored subclass of unittest.TestCase (?)
+from django.contrib.auth import get_user_model
 
 from .models import Card
 
 class CardModelTests(TestCase):
 
     def setUp(self):
-        pass
-
-        # self.user = get_user_model().objects.create_user(
-        #     username="mockuser",
-        #     password="testpass123"
-        # )
+        self.user = get_user_model().objects.create_user(
+            username="mockuser",
+            password="testpass123"
+        )
 
     def test_that_testing_works(self):
         self.assertEqual(2 + 2, 4)
+
+    def test_instantiate_card(self):
+        """Can a Card object be instantiated?"""
+        testcard = Card()
+        self.assertIsInstance(testcard, Card)
 
