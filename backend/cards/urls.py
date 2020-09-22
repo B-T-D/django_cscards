1b5e540fc4eb1af24966a9a2e2529449f23a3313
Ben <bdye71@gmail.com>
2020-09-21 20:03:35 -0400

    JWT auth working as a purely backend matter

--- backend/cards/urls.py
+++ backend/cards/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 from .apiviews import ListCards, EditCard, DeleteCard
 
 urlpatterns = [
+    path('jwtoken/', jwt_views.TokenObtainPairView.as_view(), name='jwtoken_obtain_pair'), # JSON webtoken access token
+    path('jwtoken/refresh/', jwt_views.TokenRefreshView.as_view(), name='jwtoken_refrech'), # JSON webtoken refresh token
     path('list/', ListCards.as_view()),
     path('create/', ListCards.as_view()),
     path('edit/<int:pk>/', EditCard.as_view()),
