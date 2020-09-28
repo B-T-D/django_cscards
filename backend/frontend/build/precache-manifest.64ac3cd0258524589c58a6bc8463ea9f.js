b09f34c129c06fd4c9591f60df824722a8709c30
Ben <bdye71@gmail.com>
2020-09-28 05:10:31 -0400

    gitignore fixes

---
+++ backend/frontend/build/precache-manifest.64ac3cd0258524589c58a6bc8463ea9f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffab732f236941c9796081bd11357e70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "511b95dac217e1351321",
+    "url": "/static/css/main.259d05e6.chunk.css"
+  },
+  {
+    "revision": "173e7d7b8e7bb04a3b0a",
+    "url": "/static/js/2.ee70655d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ee70655d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "511b95dac217e1351321",
+    "url": "/static/js/main.f500f3e6.chunk.js"
+  },
+  {
+    "revision": "a2305ad89c30a8f58ac5",
+    "url": "/static/js/runtime-main.b8e26a73.js"
+  },
+  {
+    "revision": "9e2ea93ce5e93132f392f8fe40ff2560",
+    "url": "/static/media/arrow-left-circle.9e2ea93c.svg"
+  },
+  {
+    "revision": "ae6178fe012be084ad1b0cddd1e49327",
+    "url": "/static/media/chevron-double-left.ae6178fe.svg"
+  },
+  {
+    "revision": "411c44fffc4569cf4d4aecab4dd82de4",
+    "url": "/static/media/chevron-double-right.411c44ff.svg"
+  },
+  {
+    "revision": "37921ea75c0963589780cce91f1cc4f3",
+    "url": "/static/media/drf_logo.37921ea7.png"
+  }
+]);
