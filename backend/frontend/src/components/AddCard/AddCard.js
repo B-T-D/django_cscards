0d2d965ed661d5b6fb6a9b76dbbef3d39a1dd2f5
Ben <bdye71@gmail.com>
2020-11-01 18:14:32 -0500

    Backup with add card modal first attempt coded but not rendered

--- backend/frontend/src/components/AddCard/AddCard.js
+++ backend/frontend/src/components/AddCard/AddCard.js
@@ -2,6 +2,77 @@ import React from 'react';
 import { CardDetailForm } from '../CardDetail/CardDetailForm';
 import { AddToggleButton } from './AddButton';
 
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+import { DynamicLabelButton } from '../ReusableButtons/DynamicLabelButton';
+
+export class AddCardModal extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        }
+
+        // Method binds
+        this.handleShow = this.handleShow.bind(this)
+        this.handleClose = this.handleClose.bind(this)
+
+    }
+
+    handleShow() {
+        this.setState(
+            {show: true}
+        )
+    }
+
+    handleClose() {
+        this.setState(
+            {show: false}
+        )
+    }
+
+   render() {
+    return(
+        <div>
+            <DynamicLabelButton
+                className={"btn btn-outline-dark"}
+                mainText={"+"}
+                hoverText={"Add new card"}
+                onClick={this.handleShow}
+                lagLeave={0}
+            />
+            <Modal
+                show={this.state.show}
+                onHide={this.handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>New card</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <CardDetailForm
+                        onSubmit={this.props.onCreateCard}
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleClose}>
+                        Close DOESNT INTERACT WITH FORM rn
+                    </Button>
+                    <Button variant="primary">
+                        One of these should be save, other should be cancel
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+        </div>
+    )
+   }
+
+}
+
 export class AddCard extends React.Component {
 
     constructor(props) {
