6618c073572cc16c22fb7b8ded67e35c8cab7350
Ben <bdye71@gmail.com>
2020-11-01 15:03:09 -0500

    Deleted unused axios import

--- backend/frontend/src/components/App/App.js
+++ backend/frontend/src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import jwt_decode from 'jwt-decode'; // TODO maybe you don't need to import it.
 
 import { Nav } from '../Nav/Nav';
